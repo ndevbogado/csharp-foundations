e3559bdcd7a739493d1a2edd57542ec314d675e0
ndevbogado <ndevbogado@gmail.com>
2023-01-11 20:43:14 -0300

    feat: initialize app 02 (methods)

---
+++ App_02_methods/App_02_methods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace App_02_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
