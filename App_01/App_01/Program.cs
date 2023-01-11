81db3dad209af873dc500987760ebd1a505bba80
ndevbogado <ndevbogado@gmail.com>
2023-01-11 14:20:27 -0300

    feat: Add App_01 project

---
+++ App_01/App_01/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+using System --> directiva para que el programa utilice el paquete 'Sytem', del cual porvienen
+                 objetos como Console, etc.
+*/
+
+namespace App_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there!");
+        }
+    }
+}
