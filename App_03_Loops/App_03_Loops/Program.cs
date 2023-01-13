3b4584f1afddc299502bfbee3d4aff3f47ad577a
ndevbogado <ndevbogado@gmail.com>
2023-01-13 14:45:43 -0300

    feat: create loops practice file

---
+++ App_03_Loops/App_03_Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace App_03_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Deseas entrar al bucle 'while'?: ");
+            string respuesta = Console.ReadLine();
+
+            while (respuesta != "no")
+            {
+                Console.WriteLine("Estás ejecutando el bucle 'while'.");
+                Console.Write("Introduce tu nombre: ");
+
+                string nombre = Console.ReadLine();
+
+                Console.WriteLine($"{nombre}, saldrás cuando contestes 'no' a la pregunta.");
+
+                respuesta = Console.ReadLine();
+
+            }
+            Console.WriteLine("Saliste del bucle 'while'.");
+        }
+    }
+}
