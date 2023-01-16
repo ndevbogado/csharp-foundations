c98565c9a507380f347aea1f2aecf97796ffc2b8
ndevbogado <ndevbogado@gmail.com>
2023-01-16 10:43:32 -0300

    feat: Create exceptions management file

---
+++ App_04_Exceptions/App_04_Exceptions/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace App_04_Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader file = null;
+
+            try
+            {
+                string line;
+
+                int contador = 0;
+
+                string path = @"C:/Users/Nahue/Desktop/text.txt";
+
+                file = new System.IO.StreamReader(path);
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    contador++;
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine($"Error en la lectura del archivo: {e.Message}");
+            } finally
+            {
+                if (file != null)
+                {
+                    file.Close();
+                    Console.WriteLine("File successfuly closed.");
+                }
+            }
+
+        }
+    }
+}
