5959fb3cc0b0bc64049b0facc1479e0da7aecfd8
ndevbogado <ndevbogado@gmail.com>
2023-01-17 11:02:05 -0300

    feat: Create POO practice file

---
+++ App_05_POO/App_05_POO/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace App_05_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo myCircle = new Circulo(); // Creación o instancia del objeto de tipo Circulo (variable del tipo objeto).
+
+            Console.WriteLine(myCircle.calculoArea(5));
+
+            Circulo myCircle2 = new Circulo();
+
+            Console.WriteLine(myCircle2.calculoArea(4));
+        }
+    }
+
+    class Circulo
+    {
+        const double PI = 3.1415; // Propiedad o 'campo de clase' de Circulo.
+
+        public double calculoArea(int radio) // Método de clase. Nos indica el comportamiento de la clase Circulo.
+        {
+            return PI * radio * radio;
+        }
+    }
+}
