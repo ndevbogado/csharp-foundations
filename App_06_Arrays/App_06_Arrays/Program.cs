1a5f5b7f4561cf338053137991b3fa87568e8051
ndevbogado <ndevbogado@gmail.com>
2023-01-18 16:53:39 -0300

    feate: Create 06_Arrays folder

---
+++ App_06_Arrays/App_06_Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace App_06_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] edades;
+
+            edades = new int[4];
+
+            edades[0] = 1;
+            edades[1] = 5;
+            edades[2] = 6;
+            edades[3] = 7;
+
+            Console.WriteLine(edades[2]);
+        }
+    }
+}
