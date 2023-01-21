d61e7a2c03f44a4a184e68f0359d81c2e11c2502
ndevbogado <ndevbogado@gmail.com>
2023-01-20 21:24:16 -0300

    feat: Add array manipulation and foreach

--- App_06_Arrays/App_06_Arrays/Program.cs
+++ App_06_Arrays/App_06_Arrays/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+
+            /*
             int[] edades;
 
             edades = new int[4];
@@ -14,6 +16,47 @@
             edades[3] = 7;
 
             Console.WriteLine(edades[2]);
+            */
+
+            //var arr = new[] { 15, 27, 2.2, 5 }; // Array implícito.
+
+            Empleado empleado1 = new ("Obi-wan Kenobi", 35);
+            Empleado empleado2 = new ("Monkey D. Luffy", 19);
+
+            var empleados = new[] {empleado1, empleado2};
+
+            /*
+            Empleado[] empleados;
+            empleados[0] = empleado1;
+            empleados[1] = empleado2;
+            */
+
+            /*
+            for (int i = 0; i < empleados.Length; i++) 
+            {
+                Console.WriteLine($"Name: {empleados[i].GetName()} / Age: {empleados[i].GetAge()}");
+                //Console.WriteLine(edades[i]);
+                //Console.WriteLine(arr[i]);
+            }
+            */
+
+            foreach (var variableForeach in empleados)
+                Console.WriteLine($"Name: {variableForeach.GetName()} - Age: {variableForeach.GetAge()}");
+        }
+
+        class Empleado 
+        {
+            public Empleado (String nombre, int edad) 
+            {
+                this.nombre = nombre;
+                this.edad = edad;
+            }
+
+            private String nombre;
+            private int edad;
+
+            public String GetName() => this.nombre;
+            public int GetAge() => this.edad;
         }
     }
 }
