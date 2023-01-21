74eaf73c86667fc0dc47ea9d96a88ae095b9197c
ndevbogado <ndevbogado@gmail.com>
2023-01-21 14:09:48 -0300

    feat: Create Inheritance folder

---
+++ App_07_Inheritance/App_07_Inheritance/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace App_07_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
