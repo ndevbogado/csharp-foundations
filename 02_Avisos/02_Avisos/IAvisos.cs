16e7b309abc68bd0ebadd22250cbd5f754ee144d
ndevbogado <ndevbogado@gmail.com>
2023-01-22 16:41:57 -0300

    feat: Create 'avisos' main project & interface

---
+++ 02_Avisos/02_Avisos/IAvisos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Avisos
+{
+    internal interface IAvisos
+    {
+        void MostarAviso();
+
+        string GetFecha();
+
+    }
+}
---
+++ 02_Avisos/02_Avisos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_Avisos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
