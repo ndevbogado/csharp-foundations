be8fca22193bba8e1e9d16c1a832e8448eb39804
ndevbogado <ndevbogado@gmail.com>
2023-01-22 16:51:35 -0300

    feat: Create 'AvisosTrafico' class

---
+++ 02_Avisos/02_Avisos/AvisosTrafico.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Avisos
+{
+    internal class AvisosTrafico : IAvisos
+    {
+        public AvisosTrafico()
+        {
+            this.remitente = "DGT";
+            this.mensaje = "Sanción cometida...";
+            this.fecha = "";
+        }
+
+        public AvisosTrafico(string remitente, string mensaje, string fecha)
+        {
+            this.remitente = remitente;
+            this.mensaje = mensaje;
+            this.fecha = fecha;
+        }
+        public string GetFecha() => fecha;
+
+        public void MostarAviso() => Console.WriteLine($"Mensaje {this.mensaje} ha sido enviado por {this.remitente} el día {this.fecha}");
+
+        private string remitente;
+        private string mensaje;
+        private string fecha;
+    }
+}
