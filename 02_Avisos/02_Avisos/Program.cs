3dc4eb0ccd61efce5f25c32a18f53f3e25f2776a
ndevbogado <ndevbogado@gmail.com>
2023-01-23 16:31:01 -0300

    feat: Add 02 practice file

--- 02_Avisos/02_Avisos/Program.cs
+++ 02_Avisos/02_Avisos/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            AvisosTrafico av1 = new();
+
+            av1.MostarAviso();
+
+            AvisosTrafico av2 = new("Jefatura de Tráfico", "Sanción de Tráfico", "01/01/23");
+            Console.WriteLine( av2.GetFecha());
+            av2.MostarAviso();
         }
     }
 }
