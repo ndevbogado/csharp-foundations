a34dc4025a4c29f14564e0541a51d69c90add277
ndevbogado <ndevbogado@gmail.com>
2023-01-23 18:43:49 -0300

    feat: Add properties concept

--- App_08_Properties/App_08_Properties/Program.cs
+++ App_08_Properties/App_08_Properties/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Empleado nahuel = new("Nahuel D. Bogado");
+
+            Console.WriteLine("Ingrese Salario: ");
+            nahuel.SALARIO = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"El salario es {nahuel.SALARIO}$");
         }
 
         class Empleado
@@ -14,6 +19,7 @@
                 this.nombre = nombre;
             }
 
+            /*
             public void setSalario(double salario)
             {
                 if (salario < 0)
@@ -24,6 +30,23 @@
                     this.salario = salario;
             }
 
+            public double getSalario() => this.salario;
+            */
+
+            private double evaluaSalario(double salario)
+            {
+                if (salario < 0)
+                    return 0;
+                else
+                    return salario;
+            }
+
+            //CREACIÓN DE PROPERTY
+            public double SALARIO
+            {
+                get => this.salario;
+                set => this.salario = evaluaSalario(value);
+            }
             private string nombre;
             private double salario;
         }
