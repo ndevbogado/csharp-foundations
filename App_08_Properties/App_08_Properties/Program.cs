ef59ef38e07ea122f0ad2a55afa6566e88543cf1
ndevbogado <ndevbogado@gmail.com>
2023-01-23 18:19:26 -0300

    feat: Create 08_Properties project

---
+++ App_08_Properties/App_08_Properties/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace App_08_Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado nahuel = new("Nahuel D. Bogado");
+        }
+
+        class Empleado
+        {
+            public Empleado(string nombre) 
+            {
+                this.nombre = nombre;
+            }
+
+            public void setSalario(double salario)
+            {
+                if (salario < 0)
+                {
+                    Console.WriteLine("El salario no puede ser negativo. Se le asignará 0 como salario.");
+                }
+                else
+                    this.salario = salario;
+            }
+
+            private string nombre;
+            private double salario;
+        }
+    }
+}
