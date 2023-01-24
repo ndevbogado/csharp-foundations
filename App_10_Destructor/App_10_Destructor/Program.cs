1a5be0bbad50d5c759faa0f344a39eb48aa59750
ndevbogado <ndevbogado@gmail.com>
2023-01-24 09:51:59 -0300

    feat: Create 10_Destructors project

---
+++ App_10_Destructor/App_10_Destructor/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace App_10_Destructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ManejoArchivos miArchivo = new();
+
+            miArchivo.mensaje();
+
+
+
+        }
+
+        class ManejoArchivos
+        {
+            StreamReader archivo = null;
+            int contador = 0;
+            string linea;
+
+            public ManejoArchivos()
+            {
+                archivo = new StreamReader(@"C:/Users/Nahue/Desktop/xd.txt");
+                while ((linea = archivo.ReadLine()) != null)
+                {
+                    Console.WriteLine(linea);
+                    contador ++;    
+                }
+            }
+            public void mensaje ()
+            {
+                Console.WriteLine($"Hay {contador} líneas.");
+            }
+
+            ~ManejoArchivos()
+            {
+                archivo.Close();
+                Console.WriteLine("El proceso finalizó. Archivo cerrado exitosamente.");
+            }
+        }
+    }
+}
