586f235e8d5159dcb1783c1f01730c789cbc1c4e
ndevbogado <ndevbogado@gmail.com>
2023-01-26 12:31:04 -0300

    feat: Work with different Queue methods

--- App_14_Collections_Queue/App_14_Collections_Queue/Program.cs
+++ App_14_Collections_Queue/App_14_Collections_Queue/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Queue<int> newQueue = new();
+            // rellenando Queue
+
+            int element;
+            do
+            {
+                Console.WriteLine("Type data to queue (Type '0' to exit enqueuement): ");
+                element = int.Parse(Console.ReadLine());
+                if (element != 0)
+                    newQueue.Enqueue(element);
+                Console.Clear();
+            } while (element != 0);
+
+            foreach(int data in newQueue)
+                Console.WriteLine(data);
+
+            for (int i = newQueue.Count; i > 0; i--)
+                newQueue.Dequeue();
+
+            Console.WriteLine("Dispalying Dequeue: ");
+            foreach(int data in newQueue)
+                Console.WriteLine(data);
         }
     }
 }
