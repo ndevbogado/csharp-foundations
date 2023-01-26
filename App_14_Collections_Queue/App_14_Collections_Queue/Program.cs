9851db397c825f62be0eda3bee543e3cec6c8403
ndevbogado <ndevbogado@gmail.com>
2023-01-26 12:09:28 -0300

    feat: Create 14_Queue project

---
+++ App_14_Collections_Queue/App_14_Collections_Queue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace App_14_Collections_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
