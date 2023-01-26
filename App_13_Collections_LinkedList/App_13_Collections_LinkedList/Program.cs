ef6d1baf86ab096c6192505d4989c3205bd5ebf9
ndevbogado <ndevbogado@gmail.com>
2023-01-26 10:48:41 -0300

    feat: Create 13_LinkedList project

---
+++ App_13_Collections_LinkedList/App_13_Collections_LinkedList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace App_13_Collections_LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
