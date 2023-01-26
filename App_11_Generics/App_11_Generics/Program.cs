44de2f49483d8880d91dfdc728a923319566474e
ndevbogado <ndevbogado@gmail.com>
2023-01-25 22:52:13 -0300

    feat: Apply generic class restriction

--- App_11_Generics/App_11_Generics/Program.cs
+++ App_11_Generics/App_11_Generics/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            almacenObjetos <Empleado> archivo = new(5);
+            // almacenObjetos <Empleado> archivo = new(5);
 
             /*
             archivo.agregar("Nahuel");
@@ -14,6 +14,7 @@
             archivo.agregar("Vin");
             */
 
+            /*
             archivo.agregar(new Empleado(1500));
             archivo.agregar(new Empleado(1200));
             archivo.agregar(new Empleado(200));
@@ -23,11 +24,69 @@
             //string nombrePersona = (string)archivo.getElemento(4); // No salta en vs , pero da un error en tiempo de ejecución debido al casting
             Empleado persona = archivo.getElemento(4);
             Console.WriteLine(persona.getSalario());
+            */
 
+            AlmacenEmpleados<Director> empleados = new(3);
+            empleados.agregar(new Director(2500));
+            empleados.agregar(new Director(3500));
+            empleados.agregar(new Director(4500));
 
-         
+            for (int i = 0; i < 3; i++) 
+            {
+                Console.WriteLine(empleados.GetEmpleado(i).GetSalario());
+            }
         }
 
+        interface IParaEmpleados
+        {
+            double GetSalario();
+        }
+        
+        class AlmacenEmpleados <T> where T: IParaEmpleados // Clase genérica que maneja cualquier tipo de objetos PERO que tengan el método GetSalario de la interfaz. 
+        {
+            public AlmacenEmpleados (int z)
+            {
+                datosEmpleado = new T[z];
+            }
+
+            public void agregar( T obj)
+            {
+                datosEmpleado[i] = obj;
+                i++;
+            }
+            public T GetEmpleado(int i) => datosEmpleado[i];
+            private int i = 0;
+            private T[] datosEmpleado;
+        }
+
+        class Director: IParaEmpleados
+        {
+            public Director(double salario) => this.salario = salario;
+
+            public double GetSalario() => salario;
+
+            private double salario;
+            
+        }
+
+        class Secretaria: IParaEmpleados
+        {
+            public Secretaria(double salario) => this.salario = salario;
+
+            public double GetSalario() => salario;
+
+            private double salario;
+        }
+
+        class Electricista: IParaEmpleados
+        {
+            public Electricista(double salario) => this.salario = salario;
+            public double GetSalario() => GetSalario();
+
+            private double salario;
+        }
+
+        /*
         class almacenObjetos <T>
         {
             public almacenObjetos(int z)
@@ -56,5 +115,7 @@
             }
             public double getSalario() => salario;
         }
+        */
     }
+        
 }
