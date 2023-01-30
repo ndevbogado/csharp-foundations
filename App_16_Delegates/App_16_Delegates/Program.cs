338a66474610ab87101ad690af468ecd360ef786
ndevbogado <ndevbogado@gmail.com>
2023-01-30 20:30:28 -0300

    feat: Add Delegates functionalities

--- App_16_Delegates/App_16_Delegates/Program.cs
+++ App_16_Delegates/App_16_Delegates/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
+            ObjetoDelegado ElDelegado = new(MensajeBienvenida.SaludoBienvenida); //Creación del objeto com oreferencia de Delegado para la llamada del método perteneceiente a la clase llamada
+            ElDelegado(); //Intancia del delegado para llamar al método
+
+            ElDelegado = new(MensajeDespedida.SaludoDespedida);
+            ElDelegado();
         }
+
+        delegate void ObjetoDelegado(); 
 
         class MensajeBienvenida
         {
@@ -12,7 +19,10 @@
             {
                 Console.WriteLine("Hello There!");
             }
+        }
 
+        class MensajeDespedida
+        {
             public static void SaludoDespedida()
             {
                 Console.WriteLine("NOS VIMOS TIRRI!");
