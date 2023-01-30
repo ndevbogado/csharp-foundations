d7fe1528ab5d6f453f00eac403fd54aaa1ce42c9
ndevbogado <ndevbogado@gmail.com>
2023-01-30 20:22:40 -0300

    feat: Create 16_Delegates project

---
+++ App_16_Delegates/App_16_Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace App_16_Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        class MensajeBienvenida
+        {
+            public static void SaludoBienvenida()
+            {
+                Console.WriteLine("Hello There!");
+            }
+
+            public static void SaludoDespedida()
+            {
+                Console.WriteLine("NOS VIMOS TIRRI!");
+            }
+        }
+
+    }
+}
