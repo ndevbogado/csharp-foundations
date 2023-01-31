bf448aee14c8af4a89814e1fbb379c6118b500bb
ndevbogado <ndevbogado@gmail.com>
2023-01-30 21:43:48 -0300

    feat: Add predicate example

--- App_16_Delegates/App_16_Delegates/Program.cs
+++ App_16_Delegates/App_16_Delegates/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Xml;
 
 namespace App_16_Delegates
 {
@@ -8,6 +10,7 @@ namespace App_16_Delegates
         {
 
             //TODO: Create a delegate predicate to find prime numbers
+            /*
             List <int>  numberList = new ();
             numberList.AddRange(new int[] {1,2,3,4,5,5,6,2,7,8,9,9,1,4,34,534,635,23,2 });
             //Declaramos delegado predicado
@@ -19,14 +22,65 @@ namespace App_16_Delegates
 
             foreach (int element in evenList)
                 Console.WriteLine(element);
+            */
+
+            Person person;
+            bool continueLoop;
+            List<Person> PersonList = new();
+            do
+            {
+                person = new Person();
+                Console.WriteLine("(Do not type to exit insertion)");
+                Console.Write("Type a name: ");
+                person.Name = Console.ReadLine();
+                Console.Write("Type an age: ");
+                try
+                {
+                    person.Age = int.Parse(Console.ReadLine());
+                }
+                catch(Exception e)
+                {
+                    person.Age = 0;
+                }
+
+                if(person.Age == 0 || person.Name == "")
+                {
+                    Console.WriteLine("Exit insertion");
+                    continueLoop = false;
+                }
+                else
+                {
+                    PersonList.Add(person);
+                    continueLoop = true;
+                }
+
+                Console.Clear();
+            } while (continueLoop);
+
+            foreach (Person element in PersonList)
+                Console.WriteLine("name: {0}, Age: {1}", element.Name, element.Age);
+            Predicate<Person> newPredicate = new(IsAdult);
+            bool exist = PersonList.Exists(newPredicate);
+
+            if (exist) Console.WriteLine("There are adults in the list");
+            else Console.WriteLine("There are no adults in the list.");
         }
 
-        static bool IsEven(int number)
+        static bool IsAdult(Person person)
         {
-            if (number % 2 == 0)
+            if (person.Age >= 18)
                 return true;
             else
                 return false;
+        }
+
+
+        class Person
+        { 
+            private string nombre;
+            private int edad;
+            public string Name { get => nombre; set => nombre = value; }
+            public int Age { get => edad; set => edad = value; }
         }
 
         
