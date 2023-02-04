f2a0c3fd5a30e6ac77ad12fe20bbfec2f9c835e3
ndevbogado <ndevbogado@gmail.com>
2023-02-04 14:47:05 -0300

    feat: Add regex manipulation

--- App_16_Delegates/App_17_Regular_Expressions/App_17_Regular_Expressions/Program.cs
+++ App_16_Delegates/App_17_Regular_Expressions/App_17_Regular_Expressions/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace App_17_Regular_Expressions
+﻿using System.Text.RegularExpressions;
+
+namespace App_17_Regular_Expressions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = "Mi numbre es Nahu D. Bog y mi número de tel es (+55) 123-1234-2134 y mi código postal es 1872";
+            string pattern = "[m|M]i";
+            Regex rx = new(pattern);
+            MatchCollection matches = rx.Matches(text);
+
+            Console.WriteLine($"Number of occurences: {matches.Count}");
+            foreach(Match match in matches) Console.WriteLine(match);
         }
     }
 }
